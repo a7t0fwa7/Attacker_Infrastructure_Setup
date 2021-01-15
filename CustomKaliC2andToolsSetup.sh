73ac2aee4acccbb04de5d639d5bfbc0b16f2cbcd
a7t0fwa7 <43460290+a7t0fwa7@users.noreply.github.com>
2021-01-15 06:34:29 -0500

    Update CustomKaliC2andToolsSetup.sh

--- CustomKaliC2andToolsSetup.sh
+++ CustomKaliC2andToolsSetup.sh
@@ -33,6 +33,13 @@ sudo apt install docker-ce docker-ce-cli containerd.io -y
 #sudo systemctl start docker
 sudo systemctl enable docker
 
+#Install MonoDevelop IDE
+sudo apt install apt-transport-https dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian vs-buster main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+sudo apt update
+sudo apt install monodevelop
+
 #Create directories
 sudo mkdir /opt/Intel-Tools
 sudo mkdir /opt/Command-and-Control

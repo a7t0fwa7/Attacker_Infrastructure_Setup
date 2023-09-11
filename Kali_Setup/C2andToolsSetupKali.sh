2f1094b26365e4f5fb7435fd7748242987309bff
a7t0fwa7 <43460290+a7t0fwa7@users.noreply.github.com>
2023-09-11 19:37:06 +0800

    Update C2andToolsSetupKali.sh
    Added H4voc

--- Kali_Setup/C2andToolsSetupKali.sh
+++ Kali_Setup/C2andToolsSetupKali.sh
@@ -244,6 +244,10 @@ sudo git clone https://github.com/BishopFox/sliver.git /opt/Command-and-Control/
 # Download Mythic C2
 sudo git clone git clone https://github.com/its-a-feature/Mythic /opt/Command-and-Control/Mythic
 
+# Download H4voc
+sudo git clone https://github.com/HavocFramework/Havoc.git /opt/Command-and-Control/H4voc
+sudo apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev python3-dev libboost-all-dev mingw-w64 nasm
+
 # Install Custom Covenant
 #sudo git clone --recurse-submodules https://github.com/ZeroPointSecurity/Covenant.git /opt/Command-and-Control/CovenantModified
 sudo git clone --recurse-submodules https://github.com/cobbr/Covenant.git /opt/Command-and-Control/CovenantModified
